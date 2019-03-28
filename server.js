08f072c5185e345cfbd6c3638816d9ac44d8f694
art1373 <48440021+art1373@users.noreply.github.com>
2019-03-29 00:30:45 +0430

    backend files

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db= knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'alibaba73',
+    database : 'smartbrain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+	res.send(database);
+})
+
+app.post('/signin',(req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) =>{register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image',(req, res) => {image.handleImage(req, res, db)})
+
+app.listen(3001, () => {
+	console.log('app ok port 3001');
+})
